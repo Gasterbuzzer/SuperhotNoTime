43d196f873eea5e53bde4fff35d4652f43e7265d
Gasterbuzzer <juliankoberg@hotmail.com>
2024-09-04 10:16:18 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using SuperhotNoTime;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperhotNoTime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SuperhotNoTime")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9eb5e0b5-f286-4256-8d88-039d075ded46")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(MainClass), "Superhot No Time", "1.0.0", "Gasterbuzzer", "https://github.com/Gasterbuzzer/SuperhotNoTime/releases/")]
+[assembly: MelonGame("SUPERHOT_Team", "SUPERHOT")]
+[assembly: MelonAuthorColor(ConsoleColor.Magenta)]
+[assembly: MelonPriority(99)]
---
+++ src/MainClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+
+namespace SuperhotNoTime
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Superhot No Time";
+        public const string Description = "Mod for setting time to a constant.";
+        public const string Author = "Gasterbuzzer";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "https://github.com/Gasterbuzzer/SuperhotNoTime/releases/";
+    }
+    public class MainClass: MelonMod
+    {
+    }
+
+
+    [HarmonyPatch(typeof(TimeControl), "LateUpdate", new Type[] { })]
+    public static class PatchTimeConstant
+    {
+
+        /// <summary>
+        /// Patches timeScale to constant 1.
+        /// </summary>
+        /// <param name="__originalMethod"> Method which was called (Used to get class type.) </param>
+        /// <param name="__instance"> Caller of function. </param>
+        private static void Postfix(MethodBase __originalMethod, object __instance)
+        {
+            Time.timeScale = 0.6f;
+        }
+    }
+}
